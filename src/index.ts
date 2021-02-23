c8756207a0944f9fdc1ba0777f9bde74e7e19747
ZYROUGE <me@zyrouge.gq>
2021-02-23 06:58:43 +0530

    Added summary (untesting)

--- src/index.ts
+++ src/index.ts
@@ -10,15 +10,14 @@ const start = async () => {
     const config = await getConfig();
     const summary: ISummary[] = [];
     for (const url of config.urls) {
-        let isUp: boolean, timetaken: number | undefined;
+        let isUp: boolean;
+        const start: number = Date.now();
         try {
-            const start = Date.now();
             await axios({
                 url: url.path,
                 method: url.type
             });
-            const end = Date.now() - start;
-            Logger.log(`Pong ${chalk.blueBright(`${url.path} (${url.type})`)} in ${chalk.grey(`${end}ms`)}`);
+            Logger.log(`Pong ${chalk.blueBright(`${url.path} (${url.type})`)} in ${chalk.grey(`${Date.now() - start}ms`)}`);
             isUp = true;
             timetaken = end;
         } catch (err) {
@@ -28,9 +27,11 @@ const start = async () => {
         summary.push({
             path: url.path,
             type: url.type.toUpperCase(),
-            timetaken, up: isUp
+            timetaken: Date.now() - start,
+            up: isUp
         });
     }
+    await renderSummary(summary);
 }
 
 start();
@@ -57,18 +58,19 @@ async function getConfig(): Promise<IConfig> {
 interface ISummary {
     path: string;
     type: string;
-    timetaken?: number;
+    timetaken: number;
     up: boolean;
 }
 
 async function renderSummary(opts: ISummary[]): Promise<void> {
-    const cp = path.join(__dirname, "..", "config.yml");
+    const temp = path.join(__dirname, "template", "summary.md");
+    const out = path.join(__dirname, "..", "summary.md");
     try {
-        const rc = await fs.readFile(cp);
-        return ymlp.parse(rc.toString());
+        const rc = await fs.readFile(temp);
+        const template = rc.toString();
+        let sum = template.replace("<!-- State -->", opts.map(x => `- \`${x.type.toUpperCase()}\` [${x.path}](${x.path}) - ${x.up ? "Up" : "Down"} (${x.timetaken})`).join("\n"));
+        await fs.writeFile(out, sum);
     } catch(err) {
-        Logger.error(`Could not parse config from ${chalk.blueBright(cp)}, reason: ${chalk.redBright(err)}`);
-        Logger.warn(`Exiting...`);
-        return process.exit(0);
+        return Logger.error(`Could not parse template from ${chalk.blueBright(rp)}, reason: ${chalk.redBright(err)}`);
     }
 }
